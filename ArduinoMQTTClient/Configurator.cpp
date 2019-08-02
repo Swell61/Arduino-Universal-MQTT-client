0c2be8503b021005f7d9fbaa5a63bb341baeb850
Samuel Bailey <samuel@bingbong.tech>
2019-08-02 23:26:04 +0100

    Started adding to configurator to load configuration from EEPROM

--- ArduinoMQTTClient/Configurator.cpp
+++ ArduinoMQTTClient/Configurator.cpp
@@ -3,5 +3,32 @@
 // 
 
 #include "Configurator.h"
+#include <EEPROM/src/EEPROM.h>
+
+void Configurator::setup() {
+	
+	// Get Arduino MAC address
+
+	// Get Arduino IP Address
+
+	// Get MQTT broker IP address
+
+	// Get number of inputs
+
+	// Get inputs
+
+	// Get num of outputs
+
+	// Get outputs
+
+}
 
 
+
+byte Configurator::getNumOfInputs(byte EEPROMPointer) {
+	return EEPROM.read(EEPROMPointer++); // Read the EEPROM at address EEPROMPointer, then increment EEPROMPointer
+}
+
+byte Configurator::getNumOfOutputs(byte EEPROMPointer) {
+	return EEPROM.read(EEPROMPointer++); // Read the EEPROM at address EEPROMPoitnter, then increment EEPROMPointer
+}
--- ArduinoMQTTClient/Configurator.h
+++ ArduinoMQTTClient/Configurator.h
@@ -8,12 +8,27 @@
 #else
 	#include "WProgram.h"
 #endif
-
 /*
 	Author: Samuel Bailey
 	Date: 30/07/2019
 	Purpose: Logic for the user conguration of the system on boot up
 */
+class Configurator {
+private:
+	int EEPROMpointer = 0;
+	byte numOfInputs;
+	byte numOfOutputs;
+	Input* inputs;
+	Output* outputs;
+	byte macAddress[6];
+	byte arduinoIP[4];
+	byte MQTTBrokerIP[4];
 
+	void setup();
+	byte getNumOfInputs(byte EEPROMPointer);
+	byte getNumOfOutputs(byte EEPROMPointer);
+	Input* loadInputsFromEEPROM(byte EEPROMPointer);
+	Output* loadOutputsFromEEPROM(byte EEPROMPointer);
+};
 #endif
 
